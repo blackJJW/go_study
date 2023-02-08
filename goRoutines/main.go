9dd79490af32bd4dc69e982203a7dcb8400156e2
blackJJW <jjinwoo92@gmail.com>
2023-02-09 01:14:07 +0900

    channel

--- goRoutines/main.go
+++ goRoutines/main.go
@@ -6,9 +6,21 @@ import (
 )
 
 func main() {
+
 	go s_Count("nico")
 	go s_Count("Lynn")
-	time.Sleep(time.Second * 5)
+
+	c := make(chan string)
+	people := [5]string{"nico", "Lynn", "A", "B", "C"}
+	for _, person := range people {
+		go is_s(person, c)
+	}
+
+	for i := 0; i < len(people); i++ {
+		fmt.Print("waiting for ", i, " ")
+		fmt.Println(<-c)
+	}
+
 }
 
 func s_Count(person string) {
@@ -16,4 +28,9 @@ func s_Count(person string) {
 		fmt.Println(person, "-- s", i)
 		time.Sleep(time.Second)
 	}
+}
+
+func is_s(person string, c chan string) {
+	time.Sleep(time.Second * 10)
+	c <- person + "------- s"
 }
