730f3b1a3882ecfd1a2b896215941e3af088b545
blackJJW <jjinwoo92@gmail.com>
2023-11-12 07:43:23 +0900

    go study

--- study/var/var_attr.go
+++ study/var/var_attr.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 func main() {
 	// 변수의 4가지 속성
 	// 1. 이름 2. 값 3. 주소 4. 타입
@@ -7,5 +9,10 @@ func main() {
 	// 주소 : 컴퓨터
 	// 타입 : 정수, 실수, 문자열 등, 사이즈는 고정
 	//	int32 : 정수형 - 32bin(4byte)
+
+	var a int16 = 3456 // a : int16 , 2byte
+	var b int8 = int8(a)
+
+	fmt.Println(a, b)
 
 }
