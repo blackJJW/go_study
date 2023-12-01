99759455f3e7aae9c7a505dce74bea6cf2de06d3
blackJJW <jjinwoo92@gmail.com>
2023-12-02 01:26:53 +0900

    manual-parse test

--- manual-parse/main.go
+++ manual-parse/main.go
@@ -18,10 +18,9 @@ type config struct {
 	printUsage bool
 }
 
-var usageString = fmt.Sprintf(`Usage : %s <integer> [-h|--help]
+var usageString = fmt.Sprintf(`Usage: %s <integer> [-h|-help]
 
-A greeter application which prints the name you entered <integer> number
-of times.
+A greeter application which prints the name you entered <integer> number of times.
 `, os.Args[0])
 
 func printUsage(w io.Writer) {
@@ -29,15 +28,37 @@ func printUsage(w io.Writer) {
 }
 
 func validateArgs(c config) error {
-	if !(c.numTimes > 0) {
+	if c.numTimes <= 0 && !c.printUsage {
 		return errors.New("Must specify a number greater than 0")
 	}
 	return nil
 }
 
+func parseArgs(args []string) (config, error) {
+	var numTimes int
+	var err error
+	c := config{}
+	if len(args) != 1 {
+		return c, errors.New("Invalid number of arguments")
+	}
+
+	if args[0] == "-h" || args[0] == "-help" {
+		c.printUsage = true
+		return c, nil
+	}
+
+	numTimes, err = strconv.Atoi(args[0])
+	if err != nil {
+		return c, err
+	}
+	c.numTimes = numTimes
+
+	return c, nil
+}
+
 func getName(r io.Reader, w io.Writer) (string, error) {
 	msg := "Your name please? Press the Enter key when done.\n"
-	fmt.Fprint(w, msg)
+	fmt.Fprintf(w, msg)
 	scanner := bufio.NewScanner(r)
 	scanner.Scan()
 	if err := scanner.Err(); err != nil {
@@ -50,37 +71,10 @@ func getName(r io.Reader, w io.Writer) (string, error) {
 	return name, nil
 }
 
-func parseArgs(args []string) (config, error) {
-	/*
-		config 타입의 객체 c를 생성하여 인수 데이터를 저장
-	*/
-	var numTimes int
-	var err error
-
-	c := config{}
-	if len(args) != 1 {
-		return c, errors.New("Invaild number of arguments")
-	}
-
-	if args[0] == "-h" || args[0] == "--help" {
-		c.printUsage = true
-		return c, nil
-	}
-
-	numTimes, err = strconv.Atoi(args[0])
-	if err != nil {
-		return c, nil
-	}
-	c.numTimes = numTimes
-
-	return c, nil
-}
-
 func greetUser(c config, name string, w io.Writer) {
 	msg := fmt.Sprintf("Nice to meet you %s\n", name)
-
 	for i := 0; i < c.numTimes; i++ {
-		fmt.Fprint(w, msg)
+		fmt.Fprintf(w, msg)
 	}
 }
 
@@ -94,9 +88,7 @@ func runCmd(r io.Reader, w io.Writer, c config) error {
 	if err != nil {
 		return err
 	}
-
 	greetUser(c, name, w)
-
 	return nil
 }
 
@@ -107,7 +99,6 @@ func main() {
 		printUsage(os.Stdout)
 		os.Exit(1)
 	}
-
 	err = validateArgs(c)
 	if err != nil {
 		fmt.Fprintln(os.Stdout, err)
